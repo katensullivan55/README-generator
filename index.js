126fecca0c5fd421cb95fa4fd773fa666f3bde70
Kate <katensullivan55@gmail.com>
2022-06-29 00:07:17 -0400

    readme questions written

--- index.js
+++ index.js
@@ -4,10 +4,66 @@ const fs = require('fs');
 const generateMarkdown = require(./generateMarkdown);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        //github username
+        {
+            name: 'githubuser',
+            type: 'input',
+            message: 'What is your github username?'
+        }
+        // repo title
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Enter the title of your project'
+        },
+        // description of repo
+        {
+            name: 'description',
+            type: 'input',
+            message: 'Describe your project - what purpose does it serve, what would a user want to get out of it, etc'
+        }
+        // installation
+        {
+            name: 'installation',
+            type: 'input',
+            message: 'Leave instructions for the user to install or implement project'
+        }
+        // usage
+        {
+            name: 'usage'
+            type: 'input'
+            message: 'How can this project be used?'
+        }
+        // contribute
+        {
+            name: 'contribute',
+            type: 'input',
+            message: 'Give instructions for coders who want to contribute'
+        }
+        // tests
+        {
+            name: 'tests',
+            type: 'input',
+            message: 'How can the user run tests oin this code?'
+        }
+        // license
+        {
+            name: 'license',
+            type: 'list',
+            message: 'Which license did you choose',
+            choices: ['MIT', 'GPL', 'Apache', 'none']
+        }
+    ]).then((answers) => {
+        writeToFile('README.md', generateMarkdown(answers))
+    })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
